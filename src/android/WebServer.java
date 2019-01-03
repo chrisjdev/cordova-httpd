b8dc8ead82395f074264f88ca5fe092717de4a90
Joey <joey@repulze.com>
2019-01-04 00:06:54 +0800

    implement error_path for android

--- src/android/WebServer.java
+++ src/android/WebServer.java
@@ -2,14 +2,32 @@ package com.rjfun.cordova.httpd;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Properties;
 
 public class WebServer extends NanoHTTPD
 {
-	public WebServer(InetSocketAddress localAddr, AndroidFile wwwroot) throws IOException {
+	private String errorPath;
+
+	public WebServer(InetSocketAddress localAddr, AndroidFile wwwroot, String errorPath) throws IOException {
 		super(localAddr, wwwroot);
+		this.errorPath = errorPath;
 	}
 
-	public WebServer(int port, AndroidFile wwwroot ) throws IOException {
+	public WebServer(int port, AndroidFile wwwroot, String errorPath) throws IOException {
 		super(port, wwwroot);
+		this.errorPath = errorPath;
+	}
+
+	@Override
+	public Response serveFile( String uri, Properties header, AndroidFile homeDir,
+							   boolean allowDirectoryListing )
+	{
+		Response res = super.serveFile(uri, header, homeDir, allowDirectoryListing);
+
+		if (res.status == HTTP_NOTFOUND) {
+			res = super.serveFile(errorPath, header, homeDir, allowDirectoryListing);
+		}
+
+		return res;
 	}
 }
